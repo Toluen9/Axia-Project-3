4261c5ef71da0e4263f02296886ec2162bfaa243
Toluen12 <ayodeletolulope43@gmail.com>
2025-03-31 20:24:13 +0100

    changes in features

--- script.js
+++ script.js
@@ -49,8 +49,8 @@ async function fetchData() {
             <div class="phtsnp">
                 <div class="photosnap">
                     <div> <p class="photo--prg">${eachData.company} </p> </div>
-                    <div> <button class="btn--new">New!</button> </div>
-                    <div>  <button class="btn--ft">Featured</button></div>
+                   ${eachData.new ? '<button class="btn--new">New!</button>' : ''}
+                ${eachData.featured ? '<button class="btn--ft">Featured</button>' : ''}
                 </div>
 
                 <h3>${eachData.position}  </h3>
@@ -61,11 +61,9 @@ async function fetchData() {
 
         </div>
         <div class="frontend">
-            <div class="frnt">${eachData.role} </div>
-            <div class="frnt">${eachData.level} </div>
-            <div class="frnt">${eachData.languages[0]}</div>
-            <div class="frnt">${eachData.languages[1]}</div>
-            <div class="frnt">${eachData.languages[2]}</div>
+             ${eachData.role ? `<div class="frnt">${eachData.role}</div>` : ''}
+             ${eachData.level ? `<div class="frnt">${eachData.level}</div>` : ''}
+             ${eachData.languages.map((language) => `<div class="frnt">${language}</div>`).join('')}
         </div>
     </div>
 </main>`
@@ -77,6 +75,50 @@ async function fetchData() {
 
 
 
+
+
+
   
   
- 
+
+
+
+// function displayData(fetchData) {
+//   console.log(fetchData);
+//   container.innerHTML = "";
+
+//   const jobListings = fetchData.map((eachData, index) => {
+//     return `
+//       <main id="job-advert">
+//         <div class="hero">
+//           <div class="photosnap--img">
+//             <div class="photosnaplogo">
+//               <img src="./img/photosnap.svg" alt="photosnap img">
+//             </div>
+//             <div class="phtsnp">
+//               <div class="photosnap">
+//                 <div>
+//                   <p class="photo--prg">${eachData.company}</p>
+//                 </div>
+//                 ${eachData.new ? '<button class="btn--new">New!</button>' : ''}
+//                 ${eachData.featured ? '<button class="btn--ft">Featured</button>' : ''}
+//               </div>
+//               <h3>${eachData.position}</h3>
+//               <div>
+//                 <pre>${eachData.postedAt} . ${eachData.contract} . ${eachData.location}</pre>
+//               </div>
+//             </div>
+//           </div>
+//           <div class="frontend">
+//             ${eachData.role ? `<div class="frnt">${eachData.role}</div>` : ''}
+//             ${eachData.level ? `<div class="frnt">${eachData.level}</div>` : ''}
+//             ${eachData.languages.map((language) => `<div class="frnt">${language}</div>`).join('')}
+//           </div>
+//         </div>
+//       </main>
+//     `;
+//   }).join('');
+
+//   container.innerHTML = jobListings;
+// }
+
